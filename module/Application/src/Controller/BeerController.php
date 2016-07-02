7c4a7f4590e645c8e7961561e706b300916f3ffb
Elton Minetto <eminetto@gmail.com>
2016-07-02 14:34:09 -0300

    Atualizando a cerveja

--- module/Application/src/Controller/BeerController.php
+++ module/Application/src/Controller/BeerController.php
@@ -66,5 +66,55 @@ class BeerController extends AbstractActionController
         return $view;
     }
 
+    public function editAction()
+    {
+        /* configura o form */
+        $form = new \Application\Form\Beer;
+        $form->get('send')->setAttribute('value', 'Edit');
+        $form->setAttribute('action', '/beer/edit');
+        /* adiciona o ID ao form */
+        $form->add([
+            'name' => 'id',
+            'type'  => 'hidden',
+        ]);
+        $view = new ViewModel(['form' => $form]);
+        $view->setTemplate('application/beer/save.phtml');
+
+        $request = $this->getRequest();
+         /* se a requisição é post os dados foram enviados via formulário*/
+        if ($request->isPost()) {
+            $beer = new \Application\Model\Beer;
+            /* configura a validação do formulário com os filtros e validators da entidade*/
+            $form->setInputFilter($beer->getInputFilter());
+            /* preenche o formulário com os dados que o usuário digitou na tela*/
+            $form->setData($request->getPost());
+            /* faz a validação do formulário*/
+            if (!$form->isValid()) {
+                return $view;
+            }
+            /* pega os dados validados e filtrados */
+            $data = $form->getData();
+            unset($data['send']);
+            /* salva a cerveja*/
+            $this->tableGateway->update($data, 'id = '.$data['id']);
+            /* redireciona para a página inicial que mostra todas as cervejas*/
+            return $this->redirect()->toUrl('/beer');
+        }
+
+        /* Se não é post deve mostrar os dados */
+        $id = $this->params()->fromRoute('id', 0);
+        $beer = $this->tableGateway->select(['id' => $id])->toArray();
+        if (count($beer) == 0) {
+            throw new \Exception("Beer not found", 404);
+        }
+
+         /* preenche o formulário com os  dados do banco de dados */
+        $form->get('id')->setValue($beer[0]['id']);
+        $form->get('name')->setValue($beer[0]['name']);
+        $form->get('style')->setValue($beer[0]['style']);
+        $form->get('img')->setValue($beer[0]['img']);
+
+        return $view;
+    }
 
 }
